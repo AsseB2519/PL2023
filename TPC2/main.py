ae2455037f23a13dbdfc3f886aeac95e598c0448
Afonso Bessa <73346904+AsseB2519@users.noreply.github.com>
2023-02-26 17:55:03 +0000

    Update main.py

--- TPC2/main.py
+++ TPC2/main.py
@@ -1,37 +1,70 @@
-def main():
+def calculate_sum(text):
+    if not text:
+        return 0
+
     sum = 0
     summing = True
-
-    print("Introduza o texto.")
-    text = input("> ")
-
     tmp_str = "0"
 
     i = 0
     while i < len(text):
         if text[i:i + 3].lower() == "off":
             summing = False
-            sum += int(tmp_str)
+            if tmp_str != "0":
+                sum += int(tmp_str)
             tmp_str = "0"
             i += 3
         elif summing != True and text[i:i + 2].lower() == "on":
             summing = True
             i += 2
         elif text[i] == "=":
-            sum += int(tmp_str)
+            if tmp_str != "0":
+                sum += int(tmp_str)
             tmp_str = "0"
-            print(sum)
             i += 1
         elif summing == True:
             if text[i].isdigit():
                 tmp_str += text[i]
             else:
-                sum += int(tmp_str)
+                if tmp_str != "0":
+                    sum += int(tmp_str)
                 tmp_str = "0"
             i += 1
         elif summing == False:
             i += 1
+    if tmp_str != "0" and summing:
+        sum += int(tmp_str)
+    return sum
+
+
+def main():
+    while True:
+        text = input("Introduza o texto (# para sair): ")
+        if text == "#":
+            break
+        result = 0
+        i = 0
+        while i < len(text):
+            if text[i] == "=":
+                partial_text = text[:i+1]
+                partial_result = calculate_sum(partial_text)
+                print(f"{partial_text} -> {partial_result}")
+                result += partial_result
+                text = text[i+1:]
+                i = 0
+            else:
+                i += 1
+        partial_result = calculate_sum(text)
+        if text != '':
+            print(f"{text} -> {partial_result}")
+        result += partial_result
+        print(f"Resultado: {result}\n")
 
 
 if __name__ == "__main__":
+    print("Bem-vindo à Calculadora de Texto!")
+    print("Digite uma sequência de caracteres com números e as palavras 'off' e 'on'")
+    print("Use o sinal '=' para indicar o fim de uma sequência e calcular o resultado")
+    print("Digite '#' para sair")
+
     main()
