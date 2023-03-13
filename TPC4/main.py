2c797458057e6ba62b185bf39eb1db8931abaef0
Afonso Bessa <73346904+AsseB2519@users.noreply.github.com>
2023-03-13 20:33:16 +0000

    Update main.py

--- TPC4/main.py
+++ TPC4/main.py
@@ -1,89 +1,241 @@
 import json
-import csv
-import os
-
-def ex1(filename):
-    with open(filename, 'r') as f:
-        linhas = f.readlines()
-
-    cabeca = linhas[0] # guardar header
-    linhas.pop(0) # eliminar header
-    if "{" not in cabeca:
-        colunas = cabeca.split(",")
-        colunas[-1] = colunas[-1].rstrip("\n")
-
-        final = []
-
-        for linha in linhas:
-            cols = linha.split(",")
-            cols[-1] = cols[-1].rstrip("\n")
-            tmp = {} # create a new dictionary in every iteration
-            ii = 0
-            for i in colunas:
-                tmp[i] = cols[ii]
-                ii += 1
-            final.append(tmp)
-
-        y = json.dumps(final, indent=4, ensure_ascii=False) # ensure_ascii=False to correctly handle non-ascii characters
-
-        output_filename = filename.split(".")[0] + ".json"
-        with open(output_filename, 'w', encoding='utf-8') as j: # specify encoding to correctly handle non-ascii characters
-            j.write(y)
-
-    print(f"Conversion completed. Check {output_filename} for the result.")
-
-def ex2(filename):
-    with open(filename, 'r') as f:
-        reader = csv.reader(f, delimiter=',')
-
-        # Extract header
-        header = next(reader)
-
-        # Initialize empty list to store records
-        records = []
-
-        # Loop through remaining rows
-        for row in reader:
-            record = {}
-            for i, value in enumerate(row):
-                # Check if current column corresponds to "Notas" field
-                if header[i] == "Notas{5}":
-                    notas = []
-                    for j in range(i, i + 5):
-                        notas.append(int(row[j]))
-                    record["Notas"] = notas
-                # Skip empty columns
-                elif value:
-                    record[header[i]] = value
-            records.append(record)
-
-    # Rename "Numero" to "Número"
-    for record in records:
-        if "Numero" in record:
-            record["Número"] = record.pop("Numero")
-
-    # Write records to output file
-    output_filename = os.path.splitext(filename)[0] + ".json"
-    with open(output_filename, 'w', encoding='utf-8') as j:
-        json.dump(records, j, ensure_ascii=False, indent=4)
-
-    print("Conversion completed. Check " + output_filename + " for the result.")
+import re
+from statistics import mean
 
 def main():
     while True:
-        print("Which file do you want to open? Enter 'exit' to quit.")
+
+        print("Which file do you want to open?")
         filename = input("> ")
-        if filename == 'exit':
-            print("Exiting the program.")
+
+        if filename == "exit":
             break
 
-        with open(filename, 'r') as f:
-            header = f.readline()
+        try:
+            with open(filename, 'r') as f:
+                linhas = f.readlines()
+        except FileNotFoundError:
+            print("File not found. Please try again.")
+            continue
 
-        if "{" in header:
-            ex2(filename)
-        else:
-            ex1(filename)
+        cabeca = linhas[0]
+        linhas.pop(0)
+        if "{" not in cabeca:
+            colunas = cabeca.split(",")
+            colunas[-1] = colunas[-1].rstrip("\n")
+
+            final = []
+            tmp = {}
+
+            for linha in linhas:
+                cols = linha.split(",")
+                cols[-1] = cols[-1].rstrip("\n")
+                ii = 0
+                for i in colunas:
+                    tmp[i] = cols[ii]
+                    ii += 1
+                final.append(tmp)
+
+            y = json.dumps(final, indent=4, ensure_ascii=False)
+
+            o_filename = filename.split(".")[0] + ".json"
+            with open(o_filename, 'w') as j:
+                j.write(y)
+
+        elif re.search(r'\{(\d+)\}', cabeca) != None:
+            colunas = cabeca.split(",")
+            colunas = colunas[:-1]
+
+            final = []
+            tmp = {}
+
+            for linha in linhas:
+                cols = linha.split(",")
+                cols[-1] = cols[-1].rstrip("\n")
+                ii = 0
+                cc = 0
+                while cc < len(colunas):
+                    if "{" not in colunas[cc]:
+                        tmp[colunas[cc]] = cols[ii]
+                        ii += 1
+                        cc += 1
+                    else:
+                        num = re.search(r"\{(.+?)\}", colunas[cc])
+                        num = int(num.group(1))
+                        content = re.search(r"(.+?)\{", colunas[cc])
+                        content = content.group(1)
+
+                        tmp[content] = []
+                        for x in range(0, num):
+                            tmp[content].append(cols[ii])
+                            ii += 1
+                            cc += 1
+
+                final.append(tmp)
+
+            y = json.dumps(final, indent=4, ensure_ascii=False)
+
+            o_filename = filename.split(".")[0] + ".json"
+            with open(o_filename, 'w') as j:
+                j.write(y)
+
+        elif re.search(r'\{(\d+(,\d+)*)\}', cabeca) != None and "::" not in cabeca:
+            colunas = []
+            temp = ""
+            skip_next = False
+
+            for i, c in enumerate(cabeca):
+                if skip_next:
+                    temp += c
+                    if c == '}':
+                        skip_next = False
+                    continue
+                elif c == '{':
+                    skip_next = True
+                    temp += c
+                elif c == ',':
+                    if skip_next:
+                        temp += c
+                    else:
+                        colunas.append(temp)
+                        temp = ""
+
+                else:
+                    temp += c
+            colunas.append(temp)
+            colunas.pop()
+            colunas.pop()
+
+            final = []
+
+            for linha in linhas:
+                tmp = {}
+                cols = linha.split(",")
+                ii = 0
+                cc = 0
+                while cc < len(colunas) - 1:
+                    if "{" not in colunas[cc]:
+                        tmp[colunas[cc]] = cols[ii]
+                        ii += 1
+                        cc += 1
+                    else:
+                        match = re.search(r'(\d+),(\d+)', colunas[cc])
+                        num_min = int(match.group(1))
+                        num_max = int(match.group(2))
+                        content = re.search(r"(.+?)\{", colunas[cc])
+                        content = content.group(1)
+
+                        tmp[content] = []
+                        for x in range(0, num_max):
+                            if x < num_min:
+                                tmp[content].append(cols[ii])
+                                ii += 1
+                                cc += 1
+                            else:
+                                if len(cols[ii]) != 0:
+                                    tmp[content].append(cols[ii])
+                                    ii += 1
+                                    cc += 1
+
+                final.append(tmp)
+
+            y = json.dumps(final, indent=4, ensure_ascii=False)
+
+            o_filename = filename.split(".")[0] + ".json"
+            with open(o_filename, 'w') as j:
+                j.write(y)
+
+        elif "::" in cabeca:
+            colunas = []
+            temp = ""
+            skip_next = False
+
+            for i, c in enumerate(cabeca):
+                if skip_next:
+                    temp += c
+                    if c == "}":
+                        skip_next = False
+                    continue
+                elif c == '{':
+                    skip_next = True
+                    temp += c
+                elif c == ',':
+                    if skip_next:
+                        temp += c
+                    else:
+                        colunas.append(temp)
+                        temp = ""
+                else:
+                    temp += c
+
+            colunas.append(temp)
+
+            colunas.pop()
+            colunas.pop()
+
+            final = []
+
+            for linha in linhas:
+                tmp = {}
+                cols = linha.split(",")
+                ii = 0
+                cc = 0
+                while cc < len(colunas) - 1:
+                    if "{" not in colunas[cc]:
+                        tmp[colunas[cc]] = cols[ii]
+                        ii += 1
+                        cc += 1
+                    else:
+                        match = re.search(r'(\d+),(\d+)', colunas[cc])
+                        num_min = int(match.group(1))
+                        num_max = int(match.group(2))
+                        content = re.search(r"(.+?)\{", colunas[cc])
+                        content = content.group(1)
+                        content2 = re.search(r"::(.+)", colunas[cc])
+                        content2 = content2.group(1)
+
+                        tmp2 = []
+                        for x in range(0, num_max):
+                            if x < num_min:
+                                tmp2.append(int(cols[ii]))
+                                ii += 1
+                                cc += 1
+                            else:
+                                if len(cols[ii]) != 0 and cols[ii] != '\n':
+                                    tmp2.append(int(cols[ii]))
+                                    ii += 1
+                                    cc += 1
+
+                        n = content + "_" + content2
+                        if content2 == "sum":
+                            tmp[n] = sum(tmp2)
+                        elif content2 == "media":
+                            tmp[n] = mean(tmp2)
+                        elif content2 == "max":
+                            tmp[n] = max(tmp2)
+                        elif content2 == "min":
+                            tmp[n] = min(tmp2)
+                        elif content2 == "range":
+                            tmp[n] = max(tmp2) - min(tmp2)
+                        elif content2 == "count":
+                            tmp[n] = len(tmp2)
+                        elif content2 == "reverse":
+                            tmp2.reverse()
+                            tmp[n] = tmp2
+                        elif content2 == "sorted":
+                            tmp2_sorted = sorted(tmp2)
+                            tmp[n] = tmp2_sorted
+                        else:
+                            tmp[n] = None
+
+                final.append(tmp)
+
+            y = json.dumps(final, indent=4, ensure_ascii=False)
+
+            o_filename = filename.split(".")[0] + ".json"
+            with open(o_filename, 'w') as j:
+                j.write(y)
+
 
 if __name__ == "__main__":
     main()
