1ff2c8a642283fa38797e53ae723c27c04fea566
Afonso Bessa <73346904+AsseB2519@users.noreply.github.com>
2023-03-14 17:32:21 +0000

    done

--- TPC5/main.py
+++ TPC5/main.py
@@ -6,22 +6,33 @@ def main():
     accepted_coins = {'5c': 0.05, '10c': 0.10, '20c': 0.20, '50c': 0.50, '1e': 1.00, '2e': 2.00}
     saldo = 0
     print("Cabine Telefónica")
+    print("Moedas Aceites -> 5c, 10c, 20c, 50c, 1e, 2e")
 
     while True:
         comando = input("> ")
 
         if re.match(r"^(?i:LEVANTAR)$", comando):
             if levantado:
-                print('maq: Auscultador já levantado!')
+                print('maq: "Auscultador já levantado!"')
                 continue
             else:
                 levantado = True
-                print('maq: Introduza moedas.')
+                print('maq: "Introduza moedas."')
         elif re.match(r"^(?i:POUSAR)$", comando):
             if levantado:
-                print('maq: troco=%.2f; Volte sempre!' % saldo)
+                coins_returned = []
+                for coin, value in sorted(accepted_coins.items(), reverse=True, key=lambda x: x[1]):
+                    while saldo >= value:
+                        saldo -= value
+                        coins_returned.append(coin)
+                coins_str = ", ".join([f"1x{c}" for c in coins_returned])
+                if saldo == 0:
+                    print(f'maq: "Volte sempre!"')
+                else:
+                    print(f'maq: "troco= {coins_str}; Volte sempre!"')
+                levantado = False
             else:
-                print('maq: Auscultador já pousado!')
+                print('maq: "Auscultador já pousado!"')
         elif re.match(r"^(?i:ABORTAR)$", comando):
             sys.exit()
         elif re.match(r"^(?i:MOEDA)", comando):
@@ -34,44 +45,49 @@ def main():
                     else:
                         saldo += accepted_coins[c]
                 if len(unaccepted_coins) != 0:
-                    print('maq: ' + ', '.join(unaccepted_coins) + ' - moeda inválida\nsaldo = %.2f' % saldo)
+                    string = 'maq: ' + '"' + ', '.join(unaccepted_coins) + ' - moeda inválida; saldo = ' + str(saldo) + 'c"'
+                    string = re.sub(r'\.', 'e', string)
+                    string = re.sub(r'€', 'c', string)
+                    print(string)
                 else:
-                    print('maq: saldo = %.2f' % saldo)
+                    string = 'maq: ' + '"' + 'saldo = ' + str(saldo) + 'c"'
+                    string = re.sub(r'\.', 'e', string)
+                    string = re.sub(r'€', 'c', string)
+                    print(string)
             else:
-                print('maq: Auscultador tem que estar levantado!')
+                print('maq: "Auscultador tem que estar levantado!"')
                 continue
         elif re.match(r"^[Tt]=", comando):
             if levantado:
                 num = re.search(r'[Tt]=(\d+)', comando).group(1)
                 if re.search(r'(\d{3})', num).group(1) in ['601', '641']:
-                    print('maq: Esse número não é permitido neste telefone. Queira discar novo número!')
+                    print('maq: "Esse número não é permitido neste telefone. Queira discar novo número!"')
                     continue
                 elif re.search(r'(\d{2})', num).group(1) == '00':
                     if saldo < 1.5:
-                        print('maq: Saldo insuficiente!')
+                        print('maq: "Saldo insuficiente!"')
                     else:
                         saldo -= 1.5
-                        print('maq: saldo = %.2f' % saldo)
+                        print('maq: "saldo = %.2f' % saldo + '"')
                 elif re.search(r'(\d{1})', num).group(1) == '2':
                     if saldo < 0.25:
-                        print('maq: Saldo insuficiente!')
+                        print('maq: "Saldo insuficiente!"')
                     else:
                         saldo -= 0.25
-                        print('maq: saldo = %.2f' % saldo)
+                        print('maq: "saldo = %.2f' % saldo + '"')
                 elif re.search(r'(\d{3})', num).group(1) == '800':
-                    print('maq: saldo = %.2f' % saldo)
+                    print('maq: "saldo = %.2f' % saldo + '"')
                 elif re.search(r'(\d{3})', num).group(1) == '808':
                     if saldo < 0.10:
-                        print('maq: Saldo insuficiente!')
+                        print('maq: "Saldo insuficiente!"')
                     else:
                         saldo -= 0.10
-                        print('maq: saldo = %.2f' % saldo)
+                        print('maq: "saldo = %.2f' % saldo + '"')
             else:
                 print("Auscultador tem que estar levantado!")
                 continue
         else:
-            print('maq: Comando inválido! Tente novamente.')
-
+            print('maq: "Comando inválido! Tente novamente."')
 
 if __name__ == "__main__":
     main()
